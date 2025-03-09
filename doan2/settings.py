ddcc29530d98f7e09d7e08d333c36ac20a9235ff
loc le <lethanhloc2612004@gmail.com>
2025-03-09 21:14:57 +0700

    a

--- doan2/settings.py
+++ doan2/settings.py
@@ -62,10 +62,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Đường dẫn chính xác
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Cần trỏ đúng đến thư mục chứa templates
         'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
 ]
+
 
 WSGI_APPLICATION = 'doan2.wsgi.application'
 
